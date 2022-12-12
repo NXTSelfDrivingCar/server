3fcc87ecb77bc25136f37cff88e86bb622847489
Jovan Mihajlović <jovanmihajlovic22@gmail.com>
2022-12-12 20:21:08 +0100

    authentication somewhat

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 var userController = require("./user/userController");
 var User = require("./user/userModel");
 var port = 5000;
@@ -6,12 +5,16 @@ var express = require("express");
 var session = require("express-session");
 var filestore = require("session-file-store")(session);
 var path = require("path");
+const { equal } = require("assert");
+const { truncate } = require("fs");
+const { getHashes } = require("crypto");
 
 /* 
 	Routes:
 	/ - test page
-	/user/register - register a user
-	/user/delete - delete a user
+	/user/register - registruj korisnika
+	/user/delete - obrisi korisnika
+  /logout - logout i izbrisi cookie
 */
 
 function printMainPage(req, res) {
@@ -20,6 +23,7 @@ function printMainPage(req, res) {
   res.write("<html><body><p>This is home page.</p></body></html>");
   res.end();
 }
+
 // register i delete vrlo mogu da se spoje kasnije
 // Ovo je ostavljeno sada ovako da bi se lakse razumelo
 function registerUser(req, res) {
@@ -67,11 +71,60 @@ function parseUser(req, res) {
   });
 }
 
+function checkAuth(req, res, next) {
+  //Za session detalje
+  console.log(req.session);
+
+  if (!req.session.user) {
+    var authHeader = req.headers.authorization;
+    var err = new Error("User not authenticated");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    next(err);
+
+    var auth = new Buffer.from(authHeader.split(' ')[1], "base64").toString().split(":");
+
+    // Reading username and password
+    var username = auth[0];
+    var password = auth[1];
+    if (username == "admin" && password == "password") {
+        req.session.user = "admin";
+        return true;
+    }else{
+      return checkAuth(req, res);
+    }
+  }else{
+    if (req.session.user == "admin") {
+      return true;
+    }else{
+      return checkAuth(req, res);
+    }
+  }
+}
+
 var server = express();
+
+server.use(session({
+  name: "session-id",
+  secret: "nxtEnter",
+  saveUninitialized: false,
+  resave: false,
+  store: new filestore()
+}));
 
 server.get("/", (req, res) => {
   printMainPage(req, res);
 });
+
+server.get("/logout", (req, res) => {
+  res.status(200).clearCookie("session-id");
+  req.session.destroy(function (err) {
+    res.redirect('/');
+  });
+})
+
+//Sve ispod ovoga zahteva auth
+server.use(checkAuth);
 
 server.get("/user/register", (req, res) => {
   registerUser(req, res);
@@ -84,6 +137,8 @@ server.get("/user/delete", (req, res) => {
 server.get("*", (req, res) => {
   res.end("Invalid request!");
 });
+
+server.use(express.static(path.join(__dirname, "public")));
 
 server.listen(port, function (err) {
   if (err) throw err;
