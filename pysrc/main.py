62656dd55f85a85b703e7924e993201ed5f4136f
Andrej Tasevski Mestrovic <akitasevski112@gmail.com>
2023-04-26 21:52:19 +0200

    Python stream service

--- pysrc/main.py
+++ pysrc/main.py
@@ -1,12 +1,54 @@
-from websocket import create_connection
+import socketio
+import asyncio
 
-ws = create_connection("ws://localhost:5000")
+from flask import Flask, request, jsonify
+from WebSocket import WebSocket as ws
 
-# print("Sending 'Hello, World'...")
-# ws.send("Hello, World")
+app = Flask(__name__)
+sio = socketio.AsyncClient()
 
-# print("Receiving...")
-# result =  ws.recv()
+WS_PORT = 5001
+HOST = 'localhost'
 
-# print ("Received '%s'" % result)
-# ws.close()
+sockets = []
+taskts = []
+
+@app.route('/api/join/client', methods=['POST'])
+async def joinClient():
+    # get token from request (x-www-form-urlencoded)
+    if request.method != 'POST':
+        return jsonify({'error' : 'Method not allowed', 'status' : 405})
+    
+    if request.headers['Content-Type'] != 'application/json':
+        return jsonify({'error' : 'Content-Type must be application/json', 'status' : 400})
+
+    request_data = request.get_json()
+    token = request_data['token']
+
+    if token == None:
+        return jsonify({'error' : 'Token is not provided', 'status' : 400})
+    
+    socket = ws()
+
+    socket.connect(HOST, WS_PORT)
+    socket.joinRoom('ai', token)
+
+    sockets.append(socket)
+
+    return jsonify({'status' : 200})
+
+async def wsClientOnStream(socket):
+    await socket.onStream()
+
+
+async def wsClientConnect(token):
+    socket = ws()
+
+    await socket.connect(HOST, WS_PORT)
+    await socket.joinRoom('ai', token)
+
+    sockets.append(socket)
+
+if __name__ ==  '__main__':
+
+    app.run(host='localhost', port=5003, debug=True)
